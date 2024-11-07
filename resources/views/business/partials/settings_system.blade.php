89222b5999b488abe617a61f29a347c53b3a5ac6
codingspider <36094177+codingspider@users.noreply.github.com>
2024-11-07 22:14:40 +0600

    currency rate setting added

--- resources/views/business/partials/settings_system.blade.php
+++ resources/views/business/partials/settings_system.blade.php
@@ -1,13 +1,13 @@
 <div class="pos-tab-content">
      <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-3">
             <div class="form-group">
                 {!! Form::label('theme_color', __('lang_v1.theme_color')); !!}
                 {!! Form::select('theme_color', $theme_colors,   $business->theme_color, 
                     ['class' => 'form-control select2', 'placeholder' => __('messages.please_select'), 'style' => 'width: 100%;']); !!}
             </div>
         </div>
-        <div class="col-sm-4">
+        <div class="col-sm-3">
             <div class="form-group">
                 @php
                     $page_entries = [25 => 25, 50 => 50, 100 => 100, 200 => 200, 500 => 500, 1000 => 1000, -1 => __('lang_v1.all')];
@@ -17,7 +17,15 @@
                     ['class' => 'form-control select2', 'style' => 'width: 100%;', 'id' => 'default_datatable_page_entries']); !!}
             </div>
         </div>
-        <div class="col-sm-4">
+        
+        <div class="col-sm-3">
+            <div class="form-group">
+            {!! Form::label('nexmo_key', 'Currency Rate' . ':') !!}
+            {!! Form::text('currency_rate', $business->currency_rate, [ 'class' => 'form-control']); !!}
+            </div>
+        </div>
+
+        <div class="col-sm-3">
             <div class="form-group">
                 <div class="checkbox">
                   <label>
